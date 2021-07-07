6ea51d23a058f2ec5ba798f3c9980ade9a4a1db0
Marcus Thorne <marcus_thorne475@hotmail.com>
2021-07-07 17:37:30 +0100

    done

--- 01-Ruby/03-Iterators-Blocks/06-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Iterators-Blocks/06-Louchebem/lib/louchebem.rb
@@ -1,3 +1,13 @@
 def louchebemize(sentence)
   # TODO: implement your louchebem translator
+  new_word = []
+
+  sentence.split.each do |word|
+    split_word = word.gsub(/\W/, "").split(/([aeiou].*)/)
+    spec_char = word.gsub(/[a-zA-Z]/, "")
+    conversion = word.length == 1 ? word : "l#{split_word[-1]}#{split_word[0]}oc#{spec_char}"
+    new_word << conversion
+  end
+
+  new_word.join(" ")
 end
