3bc85f0ba98856d79ebe1ba0b237889525f9a27e
Marcus Thorne <marcus_thorne475@hotmail.com>
2021-07-07 17:54:20 +0100

    done

--- 01-Ruby/03-Iterators-Blocks/Optional-01-Playing-with-strings/lib/palindrome.rb
+++ 01-Ruby/03-Iterators-Blocks/Optional-01-Playing-with-strings/lib/palindrome.rb
@@ -1,9 +1,7 @@
 def palindrome?(a_string)
   # TODO: check if a_string is a palindrome
-  if a_string.empty?
-    false
-  else
-    join_chars = a_string.downcase.gsub(/\W/, "").gsub(" ", "")
-    join_chars.reverse == join_chars
-  end
+  return false if a_string.empty?
+
+  join_chars = a_string.downcase.delete("- ,!'")
+  join_chars.reverse == join_chars
 end
